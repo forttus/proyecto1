9f398408b67e62257c5dca28103fde0becb6b110
forttus <fortunatozeladaargani@gmail.com>
2025-05-24 22:31:56 -0400

    hollaa33
    hola!!! soy una prueba

--- CapaPresentacion/FormularioHijos/Productos/Form1.cs
+++ CapaPresentacion/FormularioHijos/Productos/Form1.cs
@@ -16,5 +16,10 @@ namespace CapaPresentacion.FormularioHijos.Productos
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
